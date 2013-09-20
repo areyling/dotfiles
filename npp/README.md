67080d5027f5de325824776dad20652ba22de9fd
Andrew Reyling <areyling@gmail.com>
2013-09-20 18:16:37 -0400

    Added Notepad++ configuration and user-defined languages

---
+++ npp/README.md
@@ -0,0 +1,22 @@
+# dotfiles/npp
+
+## what's inside
+
+Notepad++ configuration files, user-defined language definitions and my **Androidian** theme. The theme is [loosely] based off of the [Obsidian v2 theme](http://svn.tuxfamily.org/viewvc.cgi/notepadplus_repository/trunk/PowerEditor/installer/themes/Obsidian.xml?revision=1038&view=markup) for Notepad++.
+
+## user-defined languages
+
+Included user-defined language definitions provide syntax highlighting for:
+
+- **ascx12simple**: general ASC X12 health care EDI documents
+- **ascx12extended**: general ASC X12 health care EDI documents (more keyword highlighting)
+- **autohotkey**: AutoHotKey scripts; original from [Dennis Thomann](http://www.autohotkey.com/board/topic/86937-setup-notepad-for-autohotkey/)
+- **csv**: comma-separated value documents
+- **gradle**: Gradle scripts
+- **groovy**: Groovy source files
+- **logcat**: Android logcat output; original from [TronicZomB Productions](https://github.com/TronicZomB/NppLogCat/releases)
+- **markdown**: Markdown documents; original from [robwierzbowski](https://github.com/robwierzbowski/Notepad---Markdown-Highlighting)
+- **mustache**: Mustache templates; original from [Marx Vilimpoc](http://raw.github.com/nuket/userDefineLang_Mustache/master/userDefineLang_Mustache.xml)
+- **nsf**: National Standard Format (NSF) health care EDI documents
+- **powershell**: PowerShell scripts (an alternative to the included Notepad++ PowerShell lexer)
+- **vssolution**: Visual Studio solution files; original from [CADbloke](https://gist.github.com/CADbloke/5493663)
---
+++ npp/install.ps1
@@ -0,0 +1,70 @@
+[CmdletBinding()]
+param ()
+
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+if(-not $global:nppPath) { $global:nppPath = "$(${env:ProgramFiles(x86)})\Notepad++" }
+if(-not (Test-Path $global:nppPath)) { throw 'Notepad++ not found!' }
+
+$nppUserPath = Join-Path $env:userprofile 'AppData\Roaming\Notepad++'
+if(-not (Test-Path $nppUserPath)) { &mkdir $nppUserPath | Out-Null }
+
+# install user-defined languages
+$langXmls = Get-ChildItem (Join-Path $scriptPath 'userDefineLang\*.xml') | %{
+    $path = $_.FullName
+    $xml = [xml](Get-Content $path)
+    if($xml -and $xml.NotepadPlus -and $xml.NotepadPlus.UserLang) {
+        Write-Verbose "`tLoaded Notepad++ user-defined language: $($xml.NotepadPlus.UserLang.name)"
+        $xml
+    } else {
+        Write-Error "Invalid user-defined language file: $path"
+    }
+}
+if($langXmls -and $langXmls.Count -gt 0) {
+    # build the user-defined language definition xml
+    $userLangXml = $langXmls[0]
+    $root = $userLangXml.NotepadPlus
+    $langXmls | Select -skip 1 | %{
+        $node = $root.OwnerDocument.ImportNode($_.NotepadPlus.UserLang, $true)
+        $root.AppendChild($node) | Out-Null
+    }
+    # and save it, backing up the existing definitions file if it exists
+    $path = Join-Path $nppUserPath 'userDefineLang.xml'
+    if(Test-Path $path) {
+        Write-Verbose 'Backing up the current Notepad++ user-defined language definitions file...'
+        $backupPath = [System.IO.Path]::ChangeExtension($path,'.bak')
+        if(Test-Path $backupPath) { Remove-Item -Path $backupPath -Force }
+        Rename-Item $path $backupPath -Force
+    }
+    $userLangXml.Save($path)
+    Write-Verbose "Notepad++ user-defined languages saved to: $path"
+} else {
+    Write-Verbose 'No valid user-defined language definitions found for Notepad++'
+}
+
+# install configuration/settings
+@('config.xml'
+  'contextMenu.xml'
+  'shortcuts.xml') | %{
+    $source = Join-Path $scriptPath $_
+    if(Test-Path $source) {
+        $path = Join-Path $nppUserPath $_
+        if(Test-Path $path) {
+            Write-Verbose "Backing up Notepad++ config file $($_)..."
+            $backupPath = [System.IO.Path]::ChangeExtension($path,'.bak')
+            if(Test-Path $backupPath) { Remove-Item -Path $backupPath -Force }
+            Rename-Item $path $backupPath -Force
+        }
+        Copy-Item $source -Destination $path -Force
+    } else {
+        Write-Verbose "Skipping Notepad++ config file (not found): $_"
+    }
+}
+
+# install themes
+$nppThemesPath = Join-Path $global:nppPath 'themes'
+# TODO add switch to remove existing themes (clean)
+Get-ChildItem $scriptPath -include theme.*.xml | %{
+    $name = $_.Name.Replace('theme.','')
+    Copy-Item $_.FullName -Destination (Join-Path $nppThemesPath $name) -Force
+    Write-Verbose "`tCopied Notepad++ theme: $name"
+}
